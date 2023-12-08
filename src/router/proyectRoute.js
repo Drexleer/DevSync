673bdf46e93dfc080568df171b79a09fd91f96ee
Martin Lamacchia <martinjiro22@gmail.com>
2023-12-08 19:33:53 -0300

    Haciendo el registro del proyecto para participantes

--- src/router/proyectRoute.js
+++ src/router/proyectRoute.js
@@ -2,9 +2,11 @@ const proyectRoute = require("express").Router();
 const createdProyect = require("../controllers/Proyects/createProyect");
 const getAllProyects = require("../controllers/Proyects/getAllProyects");
 const proyectById = require("../controllers/Proyects/proyectById");
+const registerProyect = require("../controllers/Proyects/registerProyect");
 
 proyectRoute.post("/create", createdProyect);
 proyectRoute.get("/", getAllProyects);
 proyectRoute.get("/:id", proyectById);
+proyectRoute.post("/register", registerProyect);
 
 module.exports = proyectRoute;
