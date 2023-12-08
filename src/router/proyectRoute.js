1605698aa6da977f5540ad38f07eecef889f32fe
Martin Lamacchia <martinjiro22@gmail.com>
2023-12-08 19:53:04 -0300

    Haciendo el borrado de usuarios de un proyecto

--- src/router/proyectRoute.js
+++ src/router/proyectRoute.js
@@ -1,5 +1,6 @@
 const proyectRoute = require("express").Router();
 const createdProyect = require("../controllers/Proyects/createProyect");
+const deleteUserProyect = require("../controllers/Proyects/deleteUserProyect");
 const getAllProyects = require("../controllers/Proyects/getAllProyects");
 const proyectById = require("../controllers/Proyects/proyectById");
 const registerProyect = require("../controllers/Proyects/registerProyect");
@@ -8,5 +9,6 @@ proyectRoute.post("/create", createdProyect);
 proyectRoute.get("/", getAllProyects);
 proyectRoute.get("/:id", proyectById);
 proyectRoute.post("/register", registerProyect);
+proyectRoute.post("/deleteUser", deleteUserProyect);
 
 module.exports = proyectRoute;
