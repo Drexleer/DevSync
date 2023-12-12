d9cece14570f9ab2289a8a27637d7b926ad294b5
Andy Ojeda <tuar_a@hotmail.com>
2023-12-12 13:31:54 -0300

    Poniendo condicional a la imagen

--- src/controllers/User/userRegister.js
+++ src/controllers/User/userRegister.js
@@ -6,9 +6,15 @@ const userRegister = async (req, res) => {
   try {
     const { name, lastName, userName, technologies, email, password } =
       req.body;
+    
+      
+      let result;
+      if (req.files && req.files.image) {
+        result = await uploadImage(req.files.image.tempFilePath);
+        console.log(result.secure_url, "aqui la imagen");
+      }
 
-    const result = await uploadImage(req.files.image.tempFilePath);
-    console.log(result.secure_url, "aqui la imagen");
+
 
     const userFound = await User.findOne({ email });
     if (userFound) {
@@ -22,7 +28,7 @@ const userRegister = async (req, res) => {
       technologies,
       email,
       password,
-      image: result.secure_url,
+      image: result? result.secure_url : null,
     });
 
     await newUser.save();
